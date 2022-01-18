48c070baa773d81070df3ffbe9cc33cdbc14a402
Ryan Schachte <code@ryan-schachte.com>
2022-01-17 17:56:02 -0700

    Updating font colors

--- src/Unsubscribe.js
+++ src/Unsubscribe.js
@@ -6,6 +6,24 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 export const unsubscribeUser = commonMiddleware(async (evt, ctx) => {
   console.log(evt.queryStringParameters);
   console.log(evt);
+  const email = evt.queryStringParameters["userMetadata"];
+
+  if (email) {
+    let bufferObj = Buffer.from(email, "base64");
+    let decodedString = bufferObj.toString("utf8").split("_");
+    console.log(`We are going to remove ${decodedString[0]} after we compare against ${decodedString[1]} in the DB`)
+
+    return {
+      statusCode: 301,
+      headers: {
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Credentials": "*",
+        Location: "https://ryan-schachte.com/unsubscribed",
+      },
+    };
+  }
 
   return {
     statusCode: 301,
@@ -14,7 +32,7 @@ export const unsubscribeUser = commonMiddleware(async (evt, ctx) => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "*",
       "Access-Control-Allow-Credentials": "*",
-      Location: "https://ryan-schachte.com/unsubscribed",
+      Location: "https://ryan-schachte.com",
     },
   };
 });
